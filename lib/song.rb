044f4eba9993bcb5680cbfd6c8f11da052ac1522
Elias Taveras <eliastave12@gmail.com>
2020-04-02 05:58:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,11 @@
 class Song
   attr_reader :name
   attr_reader :artist
+  attr_reader :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
+    @genre = genre
     
   end
 end
