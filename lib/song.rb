1287c712d4082d5bdfc2375ab3f1d67be680dae3
Elias Taveras <eliastave12@gmail.com>
2020-04-02 06:33:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,16 +26,27 @@ class Song
     @@genres.uniq
   end
   
-  def self.genre_count
-    genre_hash = {}
-    self.genres.each do |genre|
-      if genre_hash[genre]
-        genre_hash[genre] +=1
-      else
-        genre_hash = 1
-      end
-    }
-    genre_hash
-  end
+    def self.genre_count
+        genre_count = {}
+        @@genres.each do |genre|
+            if genre_count[genre]  
+                genre_count[genre] += 1
+            else
+                genre_count[genre] = 1
+            end
+        end
+        genre_count
+    end  
+  # def self.genre_count
+  #   genre_hash = {}
+  #   self.genres.each do |genre|
+  #     if genre_hash[genre]
+  #       genre_hash[genre] +=1
+  #     else
+  #       genre_hash = 1
+  #     end
+  #   }
+  #   genre_hash
+  # end
     
 end
