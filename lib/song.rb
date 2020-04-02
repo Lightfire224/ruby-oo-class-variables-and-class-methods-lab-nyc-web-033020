e107bb1e15b24205df129364dc5662bbe78d7c25
Elias Taveras <eliastave12@gmail.com>
2020-04-02 06:15:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,14 +15,14 @@ class Song
   end
     
   def self.count
-    @@count.uniq
+    @@count
   end
   
   def self.artists
-    @@artists
+    @@artists.uniq
   end
   
   def self.genres
-    @@genres
+    @@genres.uniq
   end
 end
