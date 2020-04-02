045ba3f1f9d698ff84f8322a59a65392ce4e1f54
Elias Taveras <eliastave12@gmail.com>
2020-04-02 06:36:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
     genre_hash = {}
     @@genres.each do |genre|
       if genre_hash[genre]
-          genre_hash[genre] +=1
+          genre_hash[genre] += 1
       else
           genre_hash = 1
       end
