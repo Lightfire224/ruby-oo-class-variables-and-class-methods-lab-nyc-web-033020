8dcac9d8edbdfe7db92fbfe7caf35f290c9d78e5
Elias Taveras <eliastave12@gmail.com>
2020-04-02 06:08:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,15 @@
 class Song
   attr_reader :name, :artist, :genre
   @@count = 0
+  @@artist = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     
-    @@count +=1
+    @@count += 1
+    @@artist << artist
   end
     
   def self.count
